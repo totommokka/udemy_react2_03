34ca85f5045a0927cdc3b95f875895561de97f9f
totommokka <97071781+totommokka@users.noreply.github.com>
2022-02-06 01:18:56 +0900

    Routes追加

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Link, Routes, Route, Routers } from "react-router-dom";
 
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
@@ -14,10 +14,15 @@ export default function App() {
         <Link to="/page1"> Page1 </Link>
         <br />
         <Link to="/page2"> Page2 </Link>
-        <Home />
       </div>
+      {/* v6からはSwitchではなくRoutes */}
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/page1" element={<Page1 />}></Route>
+        <Route path="/page2" element={<Page2 />}></Route>
+      </Routes>
 
-      <Switch>
+      {/* <Switch>
         <Route exact path="/">
           <Home />
         </Route>
@@ -27,7 +32,7 @@ export default function App() {
         <Route path="/page2">
           <Page2 />
         </Route>
-      </Switch>
+      </Switch> */}
     </BrowserRouter>
   );
 }
